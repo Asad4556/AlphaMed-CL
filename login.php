f9ac9a97910bcae5dce16cccc40272172f5f58ad
Asad4556 <mrasad0101@gmail.com>
2025-07-30 13:33:22 +0500

    Add files via upload

--- login.php
+++ login.php
@@ -1,17 +1,1 @@
-<?php
-session_start();
-
-$cnic = $_POST['cnic'];
-$password = $_POST['password'];
-
-$valid_cnic = '34501-8971113-7';
-$valid_password = 'Asad@2723';
-
-if ($cnic === $valid_cnic && $password === $valid_password) {
-    $_SESSION['logged_in'] = true;
-    header('Location: dashboard.php');
-    exit();
-} else {
-    echo "<script>alert('Invalid CNIC or Password'); window.location.href='login.html';</script>";
-}
-?>
+<?php session_start(); $cnic=$_POST['cnic']; $pass=$_POST['password']; if($cnic=='12345' && $pass=='test'){ $_SESSION['user']='receptionist'; header('Location: receptionist.html'); } else { echo 'Login failed'; } ?>
